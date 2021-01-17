3d4c19cb7b0eb1a1c8ffeb50d2c51962521d68de
Noam Dgani <ndgani@gmail.com>
2021-01-17 15:07:05 +0200

    sendMail function now sends mail

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,33 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'eu-west-1' });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: 'ndgani@gmail.com',
+    Destination: {
+      ToAddresses: ['ndgani@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from AWS',
+
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
+  };
+
+  try{
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  }catch(error){
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
